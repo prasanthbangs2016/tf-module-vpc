baa39733d6547b52eab83d971737effd56f9b604
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-27 02:20:19 +0530

    added vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,16 +1,35 @@
-resource "aws_subnet" "main" {
-  #subnet creation loop if it is it creates 1 if its 2 it create 2
-  count = length(var.subnets)
-  #vpc_id     = aws_vpc.main.id
-  vpc_id      = var.vpc_id
-  #cidr_block = "10.0.1.0/24"
-  cidr_block = var.subnets[count.index]
-  availability_zone = var.AZ[count.index]
+# resource "aws_subnet" "main" {
+#   #subnet creation loop if it is it creates 1 if its 2 it create 2
+#   count = length(var.subnets)
+#   #vpc_id     = aws_vpc.main.id
+#   vpc_id      = var.vpc_id
+#   #cidr_block = "10.0.1.0/24"
+#   cidr_block = var.subnets[count.index]
+#   availability_zone = var.AZ[count.index]
 
+
+#   tags = {
+#     Name = "${var.name}-subnet"
+#   }
+# }
+
+resource "aws_subnet" "main" {
+  count             = length(var.subnets)
+  vpc_id            = var.vpc_id
+  cidr_block        = var.subnets[count.index]
+  availability_zone = var.AZ[count.index]
 
   tags = {
     Name = "${var.name}-subnet"
   }
 }
 
+
+output "subnets" {
+  value = aws_subnet.main
+}
+
+output "subnet_ids" {
+  value = [for s in aws_subnet.main : s.id]
+}
 
