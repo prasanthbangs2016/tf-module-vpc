211e37d8ecfa5e701a702870633ea641d6948ffd
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-29 02:11:27 +0530

    added vpc

--- routes/main.tf
+++ routes/main.tf
@@ -8,12 +8,12 @@ resource "aws_route_table" "route_table" {
 }
 
 # # # #attaching route tables to public subnets
-# resource "aws_route_table_association" "public" {
-#   #first public,apps,db
-#   count      = length(module.subnets.ids[var.name].out[*].id)
-#   subnet_id      = element(module.subnets[var.name].out[*].id, count.index)
-#   route_table_id = aws_route_table.route_table[var.name].id
-# }
+resource "aws_route_table_association" "public" {
+  #first public,apps,db
+  count      = length(tomap(var.subnet_ids.ids[var.name].out[*].id))
+  subnet_id      = element(tomap(var.subnet_ids[var.name].out[*].id, count.index))
+  route_table_id = aws_route_table.route_table[var.name].id
+}
 
 #writing into a file
 resource "local_file" "foo" {
