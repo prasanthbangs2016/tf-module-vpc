5172eb05c0da940b3d78d836d34ef665f469e621
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-28 22:25:16 +0530

    added code

--- main.tf
+++ main.tf
@@ -40,17 +40,42 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-resource "aws_route_table" "public" {
-    for_each = var.subnets
-    #route_table_id      
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name    = "${each.value["name"]}-rt"
-    }
+#creating public route table,and attaching to subnets
+resource "aws_route" "public" {
+    route_table_id          = aws_route_table.route_table["public"].id
+    destination_cidr_block  = "0.0.0.0/0" 
+    gateway_id              = aws_internet_gateway.igw.id
+ 
 }
+
+
+# resource "aws_route" "private-apps" {
+#     for_each                = var.subnets
+#     route_table_id          = aws_route_table.route_table["public"].id
+#     destination_cidr_block  = "0.0.0.0/0" 
+#     gateway_id              = aws_internet_gateway.igw.id
+ 
+# }
+
+
+
+# resource "aws_route" "private-db" {
+#     for_each                = var.subnets
+#     route_table_id          = aws_route_table.route_table["public"].id
+#     destination_cidr_block  = "0.0.0.0/0" 
+#     gateway_id              = aws_internet_gateway.igw.id
+ 
+# }
+
+
+
 
 
 output "out" {
     value = aws_route_table.route_table["public"].id
   
+}
+
+output "subnets" {
+  value = module.subnets.subnets
 }
