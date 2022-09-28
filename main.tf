ae5eb0d50a6a8c5c5907f4c788aa2404024358a3
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-28 23:24:19 +0530

    added vpc

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "subnets" {
     #igw_id  = aws_internet_gateway.igw.id
     #route_table = aws_route_table.route_table
 }
-
+#creating internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -29,6 +29,26 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.env}-igw"
   }
 }
+
+#using this elatic ip internet will be shared to nat
+resource "aws_eip" "ngw" {
+  vpc = true
+
+}
+#creating nat gateway
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = module.subnets["public"].out[0].id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.example]
+}
+
 
 #creating table
 resource "aws_route_table" "route_table" {
@@ -61,7 +81,7 @@ resource "aws_route" "public" {
     gateway_id              = aws_internet_gateway.igw.id
  
 }
-#attaching route tables to subnets
+#attaching route tables to public subnets
 resource "aws_route_table_association" "public" {
   count      = length(module.subnets["public"].out[*].id)
   subnet_id      = element(module.subnets["public"].out[*].id, count.index)
@@ -69,11 +89,32 @@ resource "aws_route_table_association" "public" {
 }
 
 
+resource "aws_route" "apps" {
+  #since it is a list hence so
+  route_table_id          = aws_route_table.route_table["apps"].id
+  destination_cidr_block  = "0.0.0.0/0"
+  #it should go through internet gateway
+  gateway_id              = aws_internet_gateway.igw.id
+
+}
+
+resource "aws_route" "db" {
+  #since it is a list hence so
+  route_table_id          = aws_route_table.route_table["db"].id
+  destination_cidr_block  = "0.0.0.0/0"
+  #it should go through internet gateway
+  gateway_id              = aws_internet_gateway.igw.id
+
+}
+
+
+
+
 
 
 # resource "aws_route" "private-apps" {
 #     for_each                = var.subnets
-#     route_table_id          = aws_route_table.route_table["public"].id
+#     route_table_id          = aws_route_table.route_table["apps"].id
 #     destination_cidr_block  = "0.0.0.0/0"
 #     gateway_id              = aws_internet_gateway.igw.id
 #
@@ -83,7 +124,7 @@ resource "aws_route_table_association" "public" {
 #
 # resource "aws_route" "private-db" {
 #     for_each                = var.subnets
-#     route_table_id          = aws_route_table.route_table["public"].id
+#     route_table_id          = aws_route_table.route_table["db"].id
 #     destination_cidr_block  = "0.0.0.0/0"
 #     gateway_id              = aws_internet_gateway.igw.id
 #
