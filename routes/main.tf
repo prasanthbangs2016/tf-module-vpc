69ebe881dd50ff2586588120bdf46a2fe3ac6b8c
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-09-29 02:41:44 +0530

    added vpc

--- routes/main.tf
+++ routes/main.tf
@@ -12,7 +12,7 @@ resource "aws_route_table_association" "public" {
   #first public,apps,db
   #since values are hence having tomap function 
   #count      = length(tomap(var.subnet_ids.ids[var.name].out[*].id))
-  count      = length(var.subnet_ids.ids[var.name].subnet_ids)
+  count      = length(var.subnet_ids[var.name].subnet_ids)
   subnet_id      = element(var.subnet_ids[var.name].subnet_ids, count.index)
   route_table_id = aws_route_table.route_table[var.name].id
 }
