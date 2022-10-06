e041c7bd1658e79e1b7e17d9dc5a24af7b5e782d
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-06 23:54:47 +0530

    added vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "main" {
 
 
   tags = {
-    Name = "${var.name}-subnet-${element(var.AZ, count.index)}"
+    Name = "roboshop-${var.name}-subnet-${element(var.AZ, count.index)}"
   }
 }
 
