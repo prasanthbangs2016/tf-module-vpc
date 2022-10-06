d61d3a3e9114569d07280486a3aefd7abbd6bb31
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-07 02:19:19 +0530

    added vpc

--- routes/main.tf
+++ routes/main.tf
@@ -19,6 +19,6 @@ resource "aws_route_table" "route-tables" {
 #  }
 
 resource "local_file" "foo" {
-  content = length(var.subnet_ids[var.name].out[*].id)
+  content = length(var.subnet_ids[var.name].subnet_ids)
   filename = "/tmp/out"
 }
