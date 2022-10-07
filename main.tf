706d8e1e9cb2034866f59ce6e1b016c8afc0961b
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-08 01:00:55 +0530

    added vpc

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_route_table" "route-tables" {
 }
 
 output "out" {
-  value = module.subnets["public"].out[0].id
+  value = module.subnets["public"].out[*].id
 }
 
 #creating rout table , taking subnets and adding route table to subnets
