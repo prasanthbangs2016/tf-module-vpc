f910cf65fcb739ebe18f7b0863ee2075860bcf76
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-08 00:29:54 +0530

    added vpc

--- main.tf
+++ main.tf
@@ -20,6 +20,9 @@ module "subnets" {
   ngw       = try(each.value["ngw"], false)
   igw       = try(each.value["igw"], false)
   env = var.env
+  #adding igw to subnets bcz public subnets should hv then we call public subnet
+  igw_id    = aws_internet_gateway.igw.id
+#  route_tables  = aws_route_table.route-tables.*.id
 
 }
 
@@ -31,4 +34,29 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+#creating route table
+
+resource "aws_route_table" "route-tables" {
+  #how many route tables(ans"3") hence for each
+  for_each = var.subnets
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Roboshop-${each.value["name"]}-rt"
+  }
+}
+
+output "out" {
+  value = aws_route_table.route-tables
+}
+
+#adding route to route table
+
+#resource "aws_route" "public" {
+#  for_each = var.subnets
+#  route_table_id            = each.value[]
+#  destination_cidr_block    = "10.0.1.0/22"
+#  vpc_peering_connection_id = "pcx-45ff3dc1"
+#  depends_on                = [aws_route_table.testing]
+#}
 
