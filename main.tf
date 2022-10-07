792d25dca19ad41bd5547b1756717e9c4672d9b1
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-08 00:33:09 +0530

    added vpc

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_route_table" "route-tables" {
 }
 
 output "out" {
-  value = aws_route_table.route-tables
+  value = aws_route_table.route-tables["public"].id
 }
 
 #adding route to route table
