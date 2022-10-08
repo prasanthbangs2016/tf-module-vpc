ba1fbb4e17a8367c054cc4c93da99a54835b1114
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-09 00:43:29 +0530

    added vpc

--- routes/main.tf
+++ routes/main.tf
@@ -9,9 +9,14 @@ resource "aws_route_table" "route-tables" {
   }
 }
 
-resource "aws_route_table_association" "assoc" {
-  #getting list of subnets with name(public,apps,db)
-  count = length(var.subnet_ids[var.name].out[*].id)
-  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
-  route_table_id = aws_route_table.route-tables.id
+#resource "aws_route_table_association" "assoc" {
+#  #getting list of subnets with name(public,apps,db)
+#  count = length(var.subnet_ids[var.name].out[*].id)
+#  subnet_id      = element(var.subnet_ids[var.name].out[*].id, count.index )
+#  route_table_id = aws_route_table.route-tables.id
+#}
+
+resource "local_file" "foo" {
+  content  = length(var.subnet_ids[var.name].out[*].id)
+  filename = "/etc/out"
 }
